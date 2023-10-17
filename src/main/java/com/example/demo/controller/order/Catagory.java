12d6283c8cb8e17777ded18bfceda6352d0258b0
SAJID ALI <34768896+alisajids@users.noreply.github.com>
2023-10-17 16:02:35 +0530

    Create Catagory.java

---
+++ src/main/java/com/example/demo/controller/order/Catagory.java
@@ -0,0 +1,30 @@
+package demo;
+
+public class Catagory {
+
+	private Long categoryId;
+	private String categoryName;
+	
+	public Catagory(Long categoryId, String categoryName) {
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+		
+	}
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+}
