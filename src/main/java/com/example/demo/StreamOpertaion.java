727cc9d72dedf2b410c2abfacf4b3c34b7f92e20
SAJID ALI <34768896+alisajids@users.noreply.github.com>
2023-10-21 23:33:59 +0530

    Update StreamOpertaion.java

--- src/main/java/com/example/demo/StreamOpertaion.java
+++ src/main/java/com/example/demo/StreamOpertaion.java
@@ -3,42 +3,53 @@ package demo;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
-public class IDemo {
+public class StreamOperation {
 	
 	public static void main(String args[]) {
 		
 		List<Integer> list = List.of(2,3,4,50,100,40);
 				
-		int max = list.stream().max((e1,e2) -> Integer.compare(e1, e2)).orElse(0);
-		
-		int secondmax = list.stream().filter(l-> l<max).collect(Collectors.toList())
-				 .stream().max((e1,e2)->Integer.compare(e1, e2)).orElse(0);
-		
+		//Find Max 
+		int max = list.stream().max(Integer::compare).orElse(0);
 		System.out.println("MAX::"+max);
+
+		//Find second max Integer
+		int secondmax = list.stream().filter(l-> l<max).toList()
+				 .stream().max(Integer::compare).orElse(0);
+		
 		System.out.println("SECOND MAX::"+secondmax);
-		//Find second max No from List
+		
+		//Find Third max No from List
 		int thirmax =  list.stream().sorted(Comparator.reverseOrder()).limit(3).skip(2).findFirst().orElse(0);
 	
 		System.out.println("Third MAX::"+thirmax);
 		
-		
+		//----------------------------------------------------------------------------------
 		
 		List<Employee> listEmp = List.of(new Employee(1,"sajid", 50000.00),
 				new Employee(2,"sam", 60000.00),
-				new Employee(3,"salaman", 80000.00),
 				new Employee(5,"aasif", 80000.00));
    
-		  // find Max salary from Employee List
+		  // find Employee with Max salary from Employee List
 			Employee e = listEmp.stream().distinct()
 					.max((e1,e2)->Double.compare(e1.getSalary(), e2.getSalary()))
 					.orElse(null);
 			
-			 System.out.println("MAX SALARY:: "+e.getSalary());
+			 System.out.println("NAME:"+ e.getName()+"  MAX SALARY:: "+e.getSalary());
 
+			Double maxSal = listEmp.stream().mapToDouble(Employee::getSalary).max().orElse(0);
 			
+			Double secondMax = listEmp.stream().mapToDouble(Employee::getSalary)
+					.filter(salary -> salary < maxSal)
+					.max().orElse(0);
+			
+			Optional<Employee> emp = listEmp.stream().filter(employee-> employee.getSalary() == secondMax).findFirst();
+			
+			 System.out.println("NAME:"+ emp.get().getName()+"  MAX SALARY:: "+emp.get().getSalary());
 			
 	}
  		
